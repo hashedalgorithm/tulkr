cf9c5559ebf6860fdd0ad24e3f9ed8188792c6be
Sanjay Kumar Kumaravelan <sanjaykumarcena@gmail.com>
2026-02-01 10:58:34 +0100

    fix: index db handling

--- lib/indexed-db.ts
+++ lib/indexed-db.ts
@@ -1,3 +1,5 @@
+import { reject } from "lodash"
+
 type TSessionStatus = "idle" | "active" | "playing" | "paused"
 export type TSession = {
   sessionId: string
@@ -7,6 +9,7 @@ export type TSession = {
   tabId: number
   url: string
 }
+
 type TDBStatus = "ready" | "error" | "initiating"
 class IndexedDB {
   private db: IDBDatabase
@@ -24,8 +27,9 @@ class IndexedDB {
         if (db.objectStoreNames.contains(this.INDEXED_DB_STORE_NAME)) return
 
         const store = db.createObjectStore(this.INDEXED_DB_STORE_NAME, {
-          keyPath: this.INDEXED_DB_STORE_SESSIONS_KEY_ID
-        })
+          keyPath: this.INDEXED_DB_STORE_SESSIONS_KEY_ID,
+          autoIncrement: true
+        } satisfies IDBObjectStoreParameters)
 
         if (!store.indexNames.contains("tabId" satisfies keyof TSession)) {
           store.createIndex(
@@ -38,7 +42,7 @@ class IndexedDB {
           store.createIndex(
             "status" satisfies keyof TSession,
             "status" satisfies keyof TSession,
-            { unique: true }
+            { unique: false }
           )
         }
       }
@@ -63,65 +67,94 @@ class IndexedDB {
   }
 
   private async ensureReady() {
-    if (!this.db) if (!this.db) throw new Error("IndexedDB not initialized")
-  }
-
-  async createIndex(key: keyof TSession, unique: boolean = true) {
-    this.ensureReady()
-    const transaction = this.db.transaction(
-      this.INDEXED_DB_STORE_NAME,
-      "readonly"
-    )
-    const store = transaction.objectStore(this.INDEXED_DB_STORE_NAME)
-
-    store.createIndex(
-      key satisfies keyof TSession,
-      key satisfies keyof TSession,
-      { unique }
-    )
+    const status = await this.status
+    if (status !== "ready" || !this.db)
+      throw new Error("IndexedDB not initialized")
   }
 
   async get(sessionId: string): Promise<TSession> {
-    this.ensureReady()
-    const transaction = this.db.transaction(
-      this.INDEXED_DB_STORE_NAME,
-      "readonly"
-    )
-    const req = transaction
-      .objectStore(this.INDEXED_DB_STORE_NAME)
-      .get(sessionId)
-    return req.result as TSession
+    await this.ensureReady()
+
+    return new Promise((resolve, reject) => {
+      const transaction = this.db.transaction(
+        this.INDEXED_DB_STORE_NAME,
+        "readonly"
+      )
+      const req = transaction
+        .objectStore(this.INDEXED_DB_STORE_NAME)
+        .get(sessionId)
+
+      req.onsuccess = () => resolve(req.result as TSession)
+      req.onerror = () => {
+        reject(this.toError(req.error))
+      }
+      req.transaction.onabort = () => reject(this.toError(transaction.error))
+      req.transaction.onerror = () => reject(this.toError(transaction.error))
+    })
   }
 
-  async getWithIndex(index: keyof TSession): Promise<TSession[]> {
-    this.ensureReady()
-    const transaction = this.db.transaction(
-      this.INDEXED_DB_STORE_NAME,
-      "readonly"
-    )
-    const req = transaction
-      .objectStore(this.INDEXED_DB_STORE_NAME)
-      .index(index)
-      .getAll()
-    return req.result.flatMap((value) => value as TSession[])
+  async getWithIndex(
+    index: keyof TSession,
+    value: string
+  ): Promise<TSession[]> {
+    await this.ensureReady()
+
+    return new Promise((resolve, reject) => {
+      const transaction = this.db.transaction(
+        this.INDEXED_DB_STORE_NAME,
+        "readonly"
+      )
+
+      const idx = transaction
+        .objectStore(this.INDEXED_DB_STORE_NAME)
+        .index(index)
+
+      const req = idx.get(value)
+
+      req.onsuccess = () => resolve([req.result] as TSession[])
+      req.onerror = () => {
+        reject(this.toError(req.error))
+      }
+      req.transaction.onabort = () => reject(this.toError(transaction.error))
+      req.transaction.onerror = () => reject(this.toError(transaction.error))
+    })
   }
 
   async insert(value: TSession): Promise<void> {
-    this.ensureReady()
-    const transaction = this.db.transaction(
-      this.INDEXED_DB_STORE_NAME,
-      "readwrite"
-    )
-    transaction.objectStore(this.INDEXED_DB_STORE_NAME).put(value)
+    await this.ensureReady()
+    return new Promise((resolve, reject) => {
+      const transaction = this.db.transaction(
+        this.INDEXED_DB_STORE_NAME,
+        "readwrite"
+      )
+      const req = transaction.objectStore(this.INDEXED_DB_STORE_NAME).put(value)
+      req.onsuccess = () => resolve()
+      req.onerror = () => {
+        reject(this.toError(req.error))
+      }
+      req.transaction.onabort = () => reject(this.toError(transaction.error))
+      req.transaction.onerror = () => reject(this.toError(transaction.error))
+    })
   }
 
   async delete(sessionId: string): Promise<void> {
-    this.ensureReady()
-    const transaction = this.db.transaction(
-      this.INDEXED_DB_STORE_NAME,
-      "readwrite"
-    )
-    transaction.objectStore(this.INDEXED_DB_STORE_NAME).delete(sessionId)
+    await this.ensureReady()
+
+    return new Promise((resolve, reject) => {
+      const transaction = this.db.transaction(
+        this.INDEXED_DB_STORE_NAME,
+        "readwrite"
+      )
+      const req = transaction
+        .objectStore(this.INDEXED_DB_STORE_NAME)
+        .delete(sessionId)
+      req.onsuccess = () => resolve()
+      req.onerror = () => {
+        reject(this.toError(req.error))
+      }
+      req.transaction.onabort = () => reject(this.toError(transaction.error))
+      req.transaction.onerror = () => reject(this.toError(transaction.error))
+    })
   }
 
   async update(
@@ -129,7 +162,7 @@ class IndexedDB {
     key: keyof TSession,
     value: TSession[keyof TSession]
   ): Promise<void> {
-    this.ensureReady()
+    await this.ensureReady()
     const existingValue = await this.get(sessionId)
 
     if (!existingValue) {
@@ -137,7 +170,40 @@ class IndexedDB {
       return
     }
 
-    this.insert({ ...existingValue, [key]: value })
+    await this.insert({ ...existingValue, [key]: value })
+  }
+
+  toError(e: DOMException | null) {
+    if (!e) return new Error("Unknown", { cause: "Unknown IndexedDB error" })
+
+    switch (e.name) {
+      case "ConstraintError":
+        return new Error(e.message, { cause: "ConstraintError" })
+
+      case "QuotaExceededError":
+        return new Error(e.message, { cause: "QuotaExceededError" })
+
+      case "NotFoundError":
+        return new Error(e.message, { cause: "NotFoundError" })
+
+      case "AbortError":
+        return new Error(e.message, { cause: "AbortError" })
+
+      case "TransactionInactiveError":
+        return new Error(e.message, { cause: "TransactionInactiveError" })
+
+      case "InvalidStateError":
+        return new Error(e.message, { cause: "InvalidStateError" })
+
+      case "ReadOnlyError":
+        return new Error(e.message, { cause: "ReadOnlyError" })
+
+      case "VersionError":
+        return new Error(e.message, { cause: "VersionError" })
+
+      default:
+        return new Error(e.message, { cause: e.name ?? "Unknown" })
+    }
   }
 }
 
